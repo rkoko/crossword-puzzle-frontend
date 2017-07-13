cb4f8c7e8a4ed6cf43fe056961a731602fef1cf5
rkoko <rinda.ko@gmail.com>
2017-07-13 09:39:35 -0400

    match the key and submitted answer

--- api-adapter.js
+++ api-adapter.js
@@ -1,6 +1,6 @@
-function getWords(){
-  fetch('http://localhost:3000/home')
-        .then(res => res.json())
-        .then(json => display(json))
-
-}
+// function getWords(){
+//   fetch('http://localhost:3000/home')
+//         .then(res => res.json())
+//         debugger
+//         .then(json => display(json))
+// }
